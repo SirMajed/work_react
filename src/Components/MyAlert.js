addd6a74d6965cfea585554ba3f9b11a55bbf315
Majed Alhasin <sidrozzg@gmail.com>
2021-11-18 13:53:43 +0300

    fix error messages

--- src/Components/MyAlert.js
+++ src/Components/MyAlert.js
@@ -1,14 +1,12 @@
 import { Alert } from '@material-ui/lab';
- 
+
 const MyAlert = (props) => {
-    return (
-        <Alert severity={props.type} color={props.color}>
-        <p className="text-xl">{props.message.message}</p>
-        {props.message.all && props.message.all.map((fun)=>(
-          <li>{fun.name + ' - ' + fun.status}</li>
-        ))}
-      </Alert>  
-    );
-}
- 
+  return (
+    <Alert severity={props.type} color={props.color}>
+      <p className="text-xl">{props.type === 'error' ? props.message : props.message.message}</p>
+      {props.message.all && props.message.all.map((fun) => <li className="ml-3 text-lg">{fun.name + ' - ' + fun.status}</li>)}
+    </Alert>
+  );
+};
+
 export default MyAlert;
